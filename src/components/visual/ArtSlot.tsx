a9ef5e87ff909525cf8bb09de4845672a3aad618
Anh Tuan Nguyen <anhtuan.mta@gmail.com>
2026-05-26 19:25:52 +0200

    COde

--- src/components/visual/ArtSlot.tsx
+++ src/components/visual/ArtSlot.tsx
@@ -59,7 +59,7 @@ export function ArtSlot({
         <Image
           source={ART[assetKey!]}
           resizeMode="cover"
-          style={StyleSheet.absoluteFill}
+          style={styles.image}
         />
       </View>
     );
@@ -183,5 +183,14 @@ const styles = StyleSheet.create({
     backgroundColor: colors.bg,
     borderWidth: 1,
     borderColor: colors.borderSoft,
+  },
+  image: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
   },
 });
